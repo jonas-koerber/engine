35cba554f73770d04b5a0a2821f80d54a494384e
jonas-koerber <jkoerber@pm.me>
2025-05-29 20:38:07 +0200

    Update display initialization to use primary monitor's video mode for window creation

--- src/display.c
+++ src/display.c
@@ -13,14 +13,23 @@ void display_init(struct Display* display, int width, int height, const char* ti
         return;
     }
     
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
     // Set window hints
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
     
     // Create window
-    display->window = glfwCreateWindow(width, height, title, NULL, NULL);
+    display->window = glfwCreateWindow(mode->width, mode->height, display->title, primaryMonitor, NULL);
     if (display->window == NULL) {
         printf("Failed to open GLFW window\n");
         glfwTerminate();
